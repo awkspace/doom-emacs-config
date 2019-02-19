4a3dfbdfd15b63ae62e4a454b19a7451bad4648f
awk <self@awk.space>
2019-02-19 14:09:16 -0500

    Less destructive setup.sh rerun behavior

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 ## Lazy install
 
-Warning: This will replace `~/.emacs` and `~/.emacs.d` if they exist!
-
 ```bash
 wget -O- https://raw.githubusercontent.com/awkspace/doom-emacs-config/master/setup.sh | bash
 ```
--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/bash
 
-rm -rf ~/.emacs ~/.emacs.d
-git clone https://github.com/hlissner/doom-emacs ~/.emacs.d -b develop -o github
-
 if [ ! -d ~/.doom.d ]
 then
   ssh -o ConnectTimeout=3 -T git@github.com </dev/null 2>&1 | grep "^Hi awkspace!" > /dev/null
@@ -12,6 +9,14 @@ then
   else
       git clone https://github.com/awkspace/doom-emacs-config ~/.doom.d -o github
   fi
+fi
+
+if [ -d ~/.emacs.d ]
+then
+    rm -rf ~/.emacs.d/.local/packages.bak
+    mv ~/.emacs.d/.local/packages ~/.emacs.d/.local/packages.bak
+else
+    git clone https://github.com/hlissner/doom-emacs ~/.emacs.d -b develop -o github
 fi
 
 cd ~/.emacs.d && yes | make quickstart
