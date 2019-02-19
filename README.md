ac2e3a9911fdf7b250e34b3eed67905e2bfe1394
awk <self@awk.space>
2019-02-19 11:16:37 -0500

    Change setup.sh to permit reruns

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Lazy install
 
+Warning: This will replace `~/.emacs` and `~/.emacs.d` if they exist!
+
 ```bash
 wget -O- https://raw.githubusercontent.com/awkspace/doom-emacs-config/master/setup.sh | bash
 ```
--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/bash
 
+rm -rf ~/.emacs ~/.emacs.d
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d -b develop -o github
 
-ssh -o ConnectTimeout=3 -T git@github.com </dev/null 2>&1 | grep "^Hi awkspace!" > /dev/null
-if [[ $? -eq 0 ]]
+if [ ! -d ~/.doom.d ]
 then
-    git clone ssh://git@github.com/awkspace/doom-emacs-config ~/.doom.d -o github
-else
-    git clone https://github.com/awkspace/doom-emacs-config ~/.doom.d -o github
+  ssh -o ConnectTimeout=3 -T git@github.com </dev/null 2>&1 | grep "^Hi awkspace!" > /dev/null
+  if [[ $? -eq 0 ]]
+  then
+      git clone ssh://git@github.com/awkspace/doom-emacs-config ~/.doom.d -o github
+  else
+      git clone https://github.com/awkspace/doom-emacs-config ~/.doom.d -o github
+  fi
 fi
 
 cd ~/.emacs.d && yes | make quickstart
