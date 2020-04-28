02f42c013c7610c880d1d3b9f06252b144b45852
awk <self@awk.space>
2020-04-28 11:45:25 -0400

    Really, really, really reset

--- reset.sh
+++ reset.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rm -rf ~/.emacs.d/.local/{autoloads.*,elpa,straight,etc}
-yes | ~/.emacs.d/bin/doom refresh
+find ~/.emacs.d/.local -not -path "*/cache" -not -path "*/cache/*" -exec rm -rf "{}" \;
+yes | ~/.emacs.d/bin/doom install
