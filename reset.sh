e29115e98195a7f55a8525768c2ea2d6129f071f
awk <self@awk.space>
2020-06-22 09:17:55 -0400

    Add -prune to find command

--- reset.sh
+++ reset.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find ~/.emacs.d/.local -not -path "*/cache" -not -path "*/cache/*" -exec rm -rf "{}" \;
+find ~/.emacs.d/.local -not -path "*/cache" -not -path "*/cache/*" -prune -exec rm -rf "{}" \;
 yes | ~/.emacs.d/bin/doom install
