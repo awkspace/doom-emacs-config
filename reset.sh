58e87ed3c2b376a888e929e9497b953b7dad9cea
awk <self@awk.space>
2020-07-07 14:50:17 -0400

    Fix .local getting deleted on reset

--- reset.sh
+++ reset.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find ~/.emacs.d/.local -not -path "*/cache" -not -path "*/cache/*" -prune -exec rm -rf "{}" \;
+find ~/.emacs.d/.local -mindepth 1 -not -path "*/cache" -not -path "*/cache/*" -prune -exec rm -rf "{}" \;
 yes | ~/.emacs.d/bin/doom install
