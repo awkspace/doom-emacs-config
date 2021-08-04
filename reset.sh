90223b7ca13c7a0cb55a29bda36b20d5055f5dcd
awk <self@awk.space>
2021-08-04 11:48:17 -0400

    reset.sh: Delete compiled files

--- reset.sh
+++ reset.sh
@@ -4,4 +4,5 @@ find ~/.emacs.d/.local -mindepth 1 \
     -not -path "*/cache" -not -path "*/cache/*" \
     -not -path "*/etc" -not -path "*/etc/*" \
     -prune -exec rm -rf "{}" \;
+find ~/.emacs.d/ -mindepth 1 -name *.elc -exec rm -f "{}" \;
 yes | ~/.emacs.d/bin/doom install
