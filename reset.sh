f4bff6a09866934f0c0b1f3033a4abc6e4bfb7f0
awk <self@awk.space>
2021-08-05 10:37:46 -0400

    reset.sh: Pull changes

--- reset.sh
+++ reset.sh
@@ -4,5 +4,7 @@ find ~/.emacs.d/.local -mindepth 1 \
     -not -path "*/cache" -not -path "*/cache/*" \
     -not -path "*/etc" -not -path "*/etc/*" \
     -prune -exec rm -rf "{}" \;
-find ~/.emacs.d/ -mindepth 1 -name *.elc -exec rm -f "{}" \;
+find ~/.emacs.d/ -mindepth 1 -name '*.elc' -exec rm -f "{}" \;
+git -C ~/.emacs.d reset --hard HEAD
+git -C ~/.emacs.d pull
 yes | ~/.emacs.d/bin/doom install
