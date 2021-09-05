f59e70a8b8aa34c12cbd5851fe850df62461a54e
awk <self@awk.space>
2021-09-04 23:52:47 -0400

    Enable crystal

--- init.el
+++ init.el
@@ -80,7 +80,7 @@
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
-       ;;crystal           ; ruby at the speed of c
+       crystal             ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
        data                ; config/data formats
        ;;erlang            ; an elegant language for a more civilized age
