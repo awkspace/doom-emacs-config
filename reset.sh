faec2a3f522fc6d8d232c52de1943c34dc9f3e51
awk <awk@awk.space>
2023-02-13 18:03:39 -0500

    reset.sh: Ignore all files and dirs starting with etc* or cache*

--- reset.sh
+++ reset.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 find ~/.emacs.d/.local -mindepth 1 \
-    -not -path "*/cache" -not -path "*/cache/*" \
-    -not -path "*/etc" -not -path "*/etc/*" \
+    -not -path "*/cache*" \
+    -not -path "*/etc*" \
     -prune -exec rm -rf "{}" \;
 find ~/.emacs.d/ -mindepth 1 -name '*.elc' -exec rm -f "{}" \;
 git -C ~/.emacs.d reset --hard HEAD
